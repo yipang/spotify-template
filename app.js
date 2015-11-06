1efdd04af4c509fe33fc815c02420b2a95060811
Yi Pang <pangyibobo@gmail.com>
2015-11-05 17:33:11 -0800

    first

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var data;
 var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
 var myApp = angular.module('myApp', [])
+var artistUrl = 'https://api.spotify.com/v1/artists/{id}'
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   $scope.audioObject = {}
   $scope.getSongs = function() {
     $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
-      
+      data = $scope.tracks = response.tracks.items     
     })
   }
   $scope.play = function(song) {
@@ -23,6 +23,16 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
       $scope.currentSong = song
     }
   }
+  $scope.sortType = 'name'
+  $scope.setOrder = function(value) {
+    $scope.order = value
+  }
+  $scope.getArtist = function() {
+    $http.get(artistUrl).success(function(response) {
+      data = $scope.artists = response.artists
+    })
+  }
+
 })
 
 // Add tool tips to anything with a title property
